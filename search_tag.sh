308aef353318306b998f768145d7f143bdded946
qbeenslee <qbeenslee@gmail.com>
2018-07-17 21:31:10 +0800

    Fix cannot get real script file path

--- search_tag.sh
+++ search_tag.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+DIR="$(cd "$(dirname "$0")" && pwd)"
+R_DIR="$(dirname "$(readlink -n "$0")")"
+cd "$DIR/$R_DIR"
 
 scrapy crawl u2porn -a tag_name=$1
--- search_title.sh
+++ search_title.sh
@@ -1,2 +1,5 @@
-
+#!/bin/bash
+DIR="$(cd "$(dirname "$0")" && pwd)"
+R_DIR="$(dirname "$(readlink -n "$0")")"
+cd "$DIR/$R_DIR"
 scrapy crawl u2porn -a title=$1
